49ef30a7d20f3e86ecb1e44f727fe7e505553ba6
amir-pkft <amir.pkft@gmail.com>
2019-07-18 13:07:51 +0430

    complete exercise 1

--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import './usercard.css';
 
 const UserSelfCard = ({ user }) => {
-  return null;
+  console.log(user);
+  return (
+  
+  <div className="bord">
+       
+       <img src={user.image} className="img1"/>
+       <h1 className="name">{user.name}</h1>
+       <h2 className="familyname">{user.surName}</h2>
+       <b className="jensiat">male.43</b>
+
+  </div>
+  )
 };
 export default UserSelfCard;
